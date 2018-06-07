9633ea897ffc8f9993a270df0e2490cfd99d35d1
Hari7Kotha <hari7chandana@gmail.com>
2018-06-07 14:27:34 +1200

    FirstFrameworkCommit

---
+++ SpecflowTests/SpecflowPages/Utils/ConstantUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecflowPages
+{
+    public class ConstantUtils
+    {
+        //Base Url
+        public static string Url = "http://new-keys.azurewebsites.net/Account/Login";
+
+        //ScreenshotPath
+        public static string ScreenshotPath = @"C:\Users\chand\Desktop\SpecflowTests-Base\SpecflowTests\SpecflowPages\TestReports\Screenshots\";
+
+        //ExtentReportsPath
+        public static string ReportsPath = @"C:\Users\chand\Desktop\SpecflowTests-Base\SpecflowTests\SpecflowPages\TestReports\Test.html";
+
+        //ReportXML Path
+        public static string ReportXMLPath = @"C:\Users\chand\Desktop\SpecflowTests-Base\SpecflowTests\SpecflowPages\TestReports\ReportXML.xml";
+
+    }
+}
---
+++ SpecflowTests/SpecflowTests/AcceptanceTest/SpecFlowFeature1.feature
@@ -0,0 +1,14 @@
+﻿Feature: SpecFlowFeature1
+	In order to check for a property 
+	As a Property Owner
+	I want to search the property by name
+
+@mytag
+Scenario Outline: Search A Proeprty With Valid Name
+	Given I clicked on the Your Properties page
+	When I search for a property by its <Name>
+	Then I property should appear in the search list
+	
+	Examples: 
+	| Name   |
+	| Saniya |
